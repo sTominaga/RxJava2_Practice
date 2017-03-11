2322f9369038c9cdf4cb7655110c4f375630be0e
sTominaga <t.shin.luka@gmail.com>
2017-03-11 18:33:22 +0900

    わけわかんなくなってるのでとりあえずMainActicityだけ残して他削除 OkHttp+Retrofit+RxJavaができるようにする

---
+++ RxJava2Practice/app/src/main/java/com/example/ptn0411/rxjava2practice/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.ptn0411.rxjava2practice;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ptn0411 on 2017/03/07.
+ */
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}

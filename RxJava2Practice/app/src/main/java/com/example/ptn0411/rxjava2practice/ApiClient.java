47967d02bb5a7441f76112ca2fe9bdb45fd622bf
tominaga <tominaga-ptn@pivot.jp>
2017-03-13 15:49:42 +0900

    コードを整理 だいぶretrofit + okhttp on RxJava2.0だいぶわかってきた

---
+++ RxJava2Practice/app/src/main/java/com/example/ptn0411/rxjava2practice/ApiClient.java
@@ -0,0 +1,47 @@
+package com.example.ptn0411.rxjava2practice;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+import okhttp3.logging.HttpLoggingInterceptor;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+/**
+ * Created by ptn0411 on 2017/03/13.
+ */
+
+class ApiClient {
+
+    static Retrofit create() {
+
+        OkHttpClient.Builder httpClientBuilder = new OkHttpClient.Builder();
+
+        httpClientBuilder.addInterceptor(chain -> {
+            Request request = chain.request();
+
+            Request.Builder requestBuilder = request.newBuilder();
+            requestBuilder.addHeader("Accept", "application/json");
+
+            request = requestBuilder.build();
+            Response response = chain.proceed(request);
+
+            return response;
+        });
+
+        HttpLoggingInterceptor apiLog = new HttpLoggingInterceptor();
+        apiLog.setLevel(HttpLoggingInterceptor.Level.BASIC);
+
+        httpClientBuilder.addInterceptor(apiLog);
+        OkHttpClient client = httpClientBuilder.build();
+
+        return new Retrofit.Builder()
+                .baseUrl("https://api.github.com")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .client(client)
+                .build();
+    }
+
+}
